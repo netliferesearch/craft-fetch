5975409606981dd2d8177a105b5a564beba2f8d1
Hidayat Sagita <hidayat.sagita@gmail.com>
2016-08-08 18:57:59 +0700

    Add support for CURL

--- services/CraftFetchService.php
+++ services/CraftFetchService.php
@@ -9,7 +9,27 @@ class CraftFetchService extends BaseApplicationComponent
 	}
 	public function fetch($url = null)
 	{
-		$file = file_get_contents($url);
+		$file = @file_get_contents($url);
+		
+		if( $file === FALSE )
+		{
+			if( is_callable( 'curl_init' ) )
+			{
+				$curl = curl_init();
+				
+				curl_setopt( $curl, CURLOPT_AUTOREFERER, TRUE );
+				curl_setopt( $curl, CURLOPT_HEADER, 0 );
+				curl_setopt( $curl, CURLOPT_RETURNTRANSFER, 1 );
+				curl_setopt( $curl, CURLOPT_URL, $url );
+				curl_setopt( $curl, CURLOPT_FOLLOWLOCATION, TRUE );       
+				
+				$file = curl_exec( $curl );
+				    
+				curl_close( $curl );
+
+			}
+		}
+		
 		return $this->prepValue($file);
 	}
 }
